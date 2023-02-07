33bd90994bf3f43a37b35801b5513236facc34cb
Big-Fizz <balogun.fisayo@gmail.com>
2023-02-07 20:25:01 +0100

    first commit of the composable ColorPickerGame

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
+<template>
+  <ColorPickerGame v-if="currentLesson === 'color-picker-game'" />
+  <!-- <Recap_01_02_2023 v-else /> -->
+  <ColorPickerGameComposable />
+</template>
+
 <script>
+import ColorPickerGameComposable from "./components/ColorPickerGameComposable.vue";
 import ColorPickerGame from "./components/ColorPickerGame.vue";
-import Recap_01_02_2023 from "./components/Recap_01_02_2023.vue";
+// import Recap_01_02_2023 from "./components/Recap_01_02_2023.vue";
 
 export default {
-  components: { ColorPickerGame, Recap_01_02_2023 },
+  components: {
+    ColorPickerGame,
+    // Recap_01_02_2023,
+    ColorPickerGameComposable,
+  },
   data() {
     return {
       currentLesson: "color-picker-game",
@@ -11,10 +22,5 @@ export default {
   },
 };
 </script>
-
-<template>
-  <ColorPickerGame v-if="currentLesson === 'color-picker-game'" />
-  <Recap_01_02_2023 v-else />
-</template>
 
 <style scoped></style>
--- src/components/ColorPickerGame.vue
+++ src/components/ColorPickerGame.vue
@@ -18,7 +18,7 @@ export default {
 
     const matchColor = (value) => {
       // do a random color based on the array index;
-      const randomNumber = Math.floor(Math.random() * 3) + 1; //between 1 - 4
+      const randomNumber = Math.floor(Math.random() * 4); //between 1 - 4
 
       if (colors[randomNumber] === value) {
         message.value = `You win... [answer: ${colors[randomNumber]}]`;
